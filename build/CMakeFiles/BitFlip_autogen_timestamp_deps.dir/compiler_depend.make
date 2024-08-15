641c014c49bb63fb48091ae2b50f14cf1b0e1c66
Rohith-04 <rohith.narsing@gmail.com>
2024-08-15 22:54:21 +0530

    Added base class for logic gates

--- build/CMakeFiles/BitFlip_autogen_timestamp_deps.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for BitFlip_autogen_timestamp_deps.
-# This may be replaced when dependencies are built.
--- build/Desktop_Qt_6_7_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_BitFlip_FILE /home/tensa/Desktop/BitFlip/build/Desktop_Qt_6_7_2-Debug/BitFlip)
-set(__QT_DEPLOY_TARGET_BitFlip_TYPE EXECUTABLE)
