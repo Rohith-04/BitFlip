268d8f9542866f3d1838b52270321992c7c76afe
Rohith-04 <rohith.narsing@gmail.com>
2025-05-14 20:32:25 +0530

    Ignore build directory

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/CMakeFiles/BitFlip.dir/BitFlip_autogen/EWIEGA46WW/qrc_assets.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/BitFlip.dir/BitFlip_autogen/EWIEGA46WW/qrc_assets.cpp.obj: \
- C:\Users\Rahul\Desktop\BitFLip-2\build\Desktop_Qt_6_9_0_MinGW_64_bit-Debug\BitFlip_autogen\EWIEGA46WW\qrc_assets.cpp
--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/CMakeFiles/BitFlip.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for BitFlip.
-# This may be replaced when dependencies are built.
